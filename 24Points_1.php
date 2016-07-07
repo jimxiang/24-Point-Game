111db0a2a41fc1a06ae5bffbe17a1023c18bfbf5
jimxiang <jxhzr94151@gmail.com>
2016-07-07 17:56:43 +0800

    fix bug

---
+++ 24Points_1.php
@@ -0,0 +1,255 @@
+<?php
+@header('Content-type: application/json');
+
+// $point = array();
+$point = array(2, 3, 5, 9);
+$expressions = array();
+$points = array();
+
+// foreach($_POST as $key => $value)
+// {
+//  	array_push($point, $value);
+// }
+
+// if(count($point) == 4)
+// {
+// 	foreach($point as $value) {
+// 		if(!is_numeric($value) || $value < 1 || $value > 13)
+// 		{
+// 			$res = array("errCode" => 555, "msg" => "输入不合法");
+// 		}
+// 	}
+// }
+
+// else
+// {
+// 	$res = array("errCode" => 999, "msg" => "请输入四个合法整数");
+// }
+
+for($i = 0; $i < count($point); $i++)
+{
+	$p_arr = array();
+	$p_arr1 = array_merge($p_arr, array($point[$i]));
+	for($j = 0; $j < count($point); $j++)
+	{
+		if($j != $i)
+		{
+			$p_arr2 = array_merge($p_arr1, array($point[$j]));
+			for($k = 0; $k < count($point); $k++)
+			{
+				if($k != $i && $k != $j)
+				{
+					$p_arr3 = array_merge($p_arr2, array($point[$k]));
+					for($p = 0; $p < count($point); $p++)
+					{
+						if($p != $i && $p != $j && $p != $k)
+						{
+							$p_arr4 = array_merge($p_arr3, array($point[$p]));
+							array_push($points, $p_arr4);
+						}
+						
+					}
+				}
+				
+			}
+		}		
+	}
+}
+
+foreach($points as $key => $value)
+{
+	build_expression($value);
+}
+
+$res = execute_calculate();
+echo json_encode($res);
+
+function build_expression($point)
+{
+	$set = array();
+    $operators = array("+", "-", "*", "/");
+    global $expressions;
+    foreach($operators as $key => $value) 
+    {
+        $temary1 = array_merge($set, array($point[0], $value, $point[1]));
+        foreach ($operators as $key => $value) {
+            $temary2 = array_merge($temary1, array($value, $point[2]));
+            foreach ($operators as $key => $value) {
+                $temary3 = array_merge($temary2, array($value, $point[3]));
+                array_push($expressions, $temary3);
+                // 添加括号
+                $temp = $temary3;
+                array_splice($temp, 0, 0, "(");
+                array_splice($temp, 4, 0, ")");
+                array_push($expressions, $temp);
+                unset($temp);
+
+                $temp = $temary3;
+                array_splice($temp, 0, 0, "(");
+                array_splice($temp, 4, 0, ")");
+                array_push($expressions, $temp);
+                unset($temp);
+
+                $temp = $temary3;
+                array_splice($temp, 2, 0, "(");
+                array_splice($temp, 6, 0, ")");
+                array_push($expressions, $temp);
+                unset($temp);
+
+                $temp = $temary3;
+                array_splice($temp, 0, 0, "(");
+                array_splice($temp, 4, 0, ")");
+                array_splice($temp, 6, 0, "(");
+                array_splice($temp, 10, 0, ")");
+                array_push($expressions, $temp);
+                unset($temp);
+            }
+        }
+    }
+}
+
+function execute_calculate() 
+{
+    global $res, $expressions;
+	for($i = 0; $i < count($expressions); $i++) {
+		$result = calculate_point($expressions[$i]);
+		$point = $result[0];
+		if($point == 24) {
+			return array("errCode" => 200, "msg" => $expressions[$i]);
+		}
+	}
+	return array("errCode" => 400, "msg" => "无解");
+}
+
+function calculate_point($arr)
+{
+	// 处理括号
+    if(in_array("(", $arr) || in_array(")", $arr))
+    {
+       for($i = 0; $i < count($arr); $i++)
+       {
+           if($arr[$i] == "(" && $arr[$i+4] == ")")
+           {
+               $temp = 0;
+               switch($arr[$i+2])
+               {
+                   case "+":
+                       $temp = ($arr[$i+1]) + ($arr[$i+3]);
+                       break;
+                   case "-":
+                       $temp = ($arr[$i+1]) - ($arr[$i+3]);
+                       break;
+                   case "*":
+                       $temp = ($arr[$i+1]) * ($arr[$i+3]);
+                       break;
+                   case "/":
+                       $temp = ($arr[$i+1]) / ($arr[$i+3]);
+               }
+               $suffix = $i < (count($arr) - 1) ? array_slice($arr, $i+5) : NULL;
+               $prefix = $i > 0 ? array_slice($arr, 0, $i) : NULL;
+               if($suffix != NULL && $prefix != NULL)
+               {
+                   $arr = array_merge(array_merge($prefix, array($temp)), $suffix);
+               }
+               elseif($prefix == NULL && $suffix != NULL)
+               {
+                   $arr = array_merge(array($temp), $suffix);
+               }
+               elseif($suffix == NULL && $prefix != NULL)
+               {
+                   $arr = array_merge($prefix, array($temp));
+               }
+           }
+       }
+    }
+	while(count($arr) > 1)
+   	{
+   		while(in_array("(", $arr) || in_array(")", $arr))
+	    {
+	       for($i = 0; $i < count($arr); $i++)
+	       {
+	           if($arr[$i] == "(" && $arr[$i+4] == ")")
+	           {
+	               $temp = 0;
+	               switch($arr[$i+2])
+	               {
+	                   case "+":
+	                       $temp = ($arr[$i+1]) + ($arr[$i+3]);
+	                       break;
+	                   case "-":
+	                       $temp = ($arr[$i+1]) - ($arr[$i+3]);
+	                       break;
+	                   case "*":
+	                       $temp = ($arr[$i+1]) * ($arr[$i+3]);
+	                       break;
+	                   case "/":
+	                       $temp = ($arr[$i+1]) / ($arr[$i+3]);
+	               }
+	               $suffix = $i < (count($arr) - 1) ? array_slice($arr, $i+5) : NULL;
+	               $prefix = $i > 0 ? array_slice($arr, 0, $i) : NULL;
+	               if($suffix != NULL && $prefix != NULL)
+	               {
+	                   $arr = array_merge(array_merge($prefix, array($temp)), $suffix);
+	               }
+	               elseif($prefix == NULL && $suffix != NULL)
+	               {
+	                   $arr = array_merge(array($temp), $suffix);
+	               }
+	               elseif($suffix == NULL && $prefix != NULL)
+	               {
+	                   $arr = array_merge($prefix, array($temp));
+	               }
+	           }
+	       }
+	    }
+
+		if(in_array("*", $arr) || in_array("/", $arr))
+		{
+           // 优先处理乘法和除法
+			for($i = 0; $i < count($arr); $i++)
+			{
+				if($arr[$i] == "*")
+				{
+					$temp = ($arr[$i-1]) * ($arr[$i+1]);
+					$suffix = array_slice($arr, $i+2);
+					$prefix = array_slice($arr, 0, $i-1);
+					$arr = array_merge(array_merge($prefix, array($temp)), $suffix);
+
+				}
+
+				if($arr[$i] == "/" && ($arr[$i+1]) != 0)
+				{
+					$temp = ($arr[$i-1]) / ($arr[$i+1]);
+					$suffix = array_slice($arr, $i+2);
+					$prefix = array_slice($arr, 0, $i-1);
+					$arr = array_merge(array_merge($prefix, array($temp)), $suffix);
+				}
+			}
+		}
+
+		else
+		{
+			// 这里只剩加法和减法
+			for($i = 0; $i < count($arr); $i++)
+			{
+				if($arr[$i] == "+")
+				{
+					$temp = ($arr[$i-1]) + ($arr[$i+1]);
+					$suffix = array_slice($arr, $i+2);
+					$prefix = array_slice($arr, 0, $i-1);
+					$arr = array_merge(array_merge($prefix, array($temp)), $suffix);
+					calculate_point($arr);
+				}
+
+				if($arr[$i] == "-")
+				{
+					$temp = ($arr[$i-1]) - ($arr[$i+1]);
+					$suffix = array_slice($arr, $i+2);
+					$prefix = array_slice($arr, 0, $i-1);
+					$arr = array_merge(array_merge($prefix, array($temp)), $suffix);
+				}
+			}
+		}
+	}
+	return $arr;
+}
